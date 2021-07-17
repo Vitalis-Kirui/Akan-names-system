62775a022beb7441153b532e0123d6f6b1107e86
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-17 11:02:23 +0300

    pushing js function to collect data from the form

---
+++ index.js
@@ -0,0 +1,14 @@
+// On submit function
+
+var fetchData = function(){
+
+    var name = document.getElementById("userName").value;
+
+    var DD = document.getElementById("dateOnly").value;
+
+    var MM = document.getElementById("monthOnly").value;
+
+    var fullYear = document.getElementById("yearOnly").value;
+
+
+}
