ce59a8db152bcd6bd0396400885b3e558c48ac57
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-17 12:17:07 +0300

    pushing validation for raddio buttons controls

--- index.js
+++ index.js
@@ -11,4 +11,38 @@ var fetchData = function(){
     var fullYear = document.getElementById("yearOnly").value;
 
 
+    // spliting full-year into century and year
+
+    var digits = Array.from(String(fullYear), Number);
+
+    var century = (digits.slice(0,2));
+
+    var year = (digits.slice(2,4));
+
+    var CC = parseInt(century.join(""));
+
+    var YY = parseInt(year.join(""));
+
+    var day = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7
+
+    // alert("Name: "+ name + " Date of Birth: "+DD+"/"+MM+"/"+CC+YY+" Day of Birth: "+day);
+
+    // radio button controls validation
+
+    var male = document.getElementById("maleGender").checked;
+
+    var female = document.getElementById("femaleGender").checked;
+    
+    if(male==true){
+        alert("You are a male")
+    }
+
+    else if(female==true){
+        alert("You are a female")
+    }
+
+    else{
+        alert("Please select your gender")
+    }
+
 }
