bee463cb74bcc2781e4f0a0997fe8628112761b3
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-18 14:03:42 +0300

    pushing updates on slicing full year to century and year

--- index.js
+++ index.js
@@ -13,23 +13,15 @@ var fetchData = function(){
 
     // spliting full-year into century and year
 
-    var digits = Array.from(String(fullYear), Number);
+    var fullYearString = fullYear.toString();
 
-    var century = (digits.slice(0,2));
+    var CC = parseInt(fullYearString.slice(0,2));
 
-    var year = (digits.slice(2,4));
-
-    var CC = parseInt(century.join(""));
-
-    var YY = parseInt(year.join(""));
+    var YY = parseInt(fullYearString.slice(2,4));
 
     var day = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7
 
     var birthDay = parseInt(day);
-
-    // alert("Your born day is: "+day.toFixed());
-
-    // alert("Name: "+ name + " Date of Birth: "+DD+"/"+MM+"/"+CC+YY+" Day of Birth: "+day);
 
     // radio button controls validation
 
